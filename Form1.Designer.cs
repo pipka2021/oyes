c73bff7c90c11c1e56af3479970034930d6eeffc
pipka2021 <pipka_2021@hotmail.com>
2021-09-25 11:31:23 +0300

    123

---
+++ Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace PhotoSmort
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.открытьФотоToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.закрытьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.Next_button = new System.Windows.Forms.Button();
+            this.Prev_button = new System.Windows.Forms.Button();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel4 = new System.Windows.Forms.TableLayoutPanel();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.закрытьВсеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.tableLayoutPanel4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(788, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "Menu";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.открытьФотоToolStripMenuItem,
+            this.закрытьToolStripMenuItem,
+            this.закрытьВсеToolStripMenuItem});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // открытьФотоToolStripMenuItem
+            // 
+            this.открытьФотоToolStripMenuItem.Name = "открытьФотоToolStripMenuItem";
+            this.открытьФотоToolStripMenuItem.Size = new System.Drawing.Size(194, 22);
+            this.открытьФотоToolStripMenuItem.Text = "Открыть...";
+            this.открытьФотоToolStripMenuItem.Click += new System.EventHandler(this.открытьФотоToolStripMenuItem_Click);
+            // 
+            // закрытьToolStripMenuItem
+            // 
+            this.закрытьToolStripMenuItem.Name = "закрытьToolStripMenuItem";
+            this.закрытьToolStripMenuItem.Size = new System.Drawing.Size(194, 22);
+            this.закрытьToolStripMenuItem.Text = "Закрыть выбранное...";
+            this.закрытьToolStripMenuItem.Click += new System.EventHandler(this.закрытьToolStripMenuItem_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel3, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel4, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 24);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 120F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 35F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(788, 540);
+            this.tableLayoutPanel1.TabIndex = 2;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 5;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 80F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 5F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 80F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Controls.Add(this.Next_button, 3, 0);
+            this.tableLayoutPanel2.Controls.Add(this.Prev_button, 1, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 505);
+            this.tableLayoutPanel2.Margin = new System.Windows.Forms.Padding(3, 0, 3, 3);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(782, 32);
+            this.tableLayoutPanel2.TabIndex = 0;
+            // 
+            // Next_button
+            // 
+            this.Next_button.Location = new System.Drawing.Point(396, 0);
+            this.Next_button.Margin = new System.Windows.Forms.Padding(3, 0, 3, 3);
+            this.Next_button.Name = "Next_button";
+            this.Next_button.Size = new System.Drawing.Size(74, 23);
+            this.Next_button.TabIndex = 0;
+            this.Next_button.Text = "Next";
+            this.Next_button.UseVisualStyleBackColor = true;
+            // 
+            // Prev_button
+            // 
+            this.Prev_button.Location = new System.Drawing.Point(311, 0);
+            this.Prev_button.Margin = new System.Windows.Forms.Padding(3, 0, 3, 3);
+            this.Prev_button.Name = "Prev_button";
+            this.Prev_button.Size = new System.Drawing.Size(74, 23);
+            this.Prev_button.TabIndex = 1;
+            this.Prev_button.Text = "Prev";
+            this.Prev_button.UseVisualStyleBackColor = true;
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.ColumnCount = 3;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 502F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(3, 388);
+            this.tableLayoutPanel3.Margin = new System.Windows.Forms.Padding(3, 3, 3, 0);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 1;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(782, 117);
+            this.tableLayoutPanel3.TabIndex = 1;
+            // 
+            // tableLayoutPanel4
+            // 
+            this.tableLayoutPanel4.ColumnCount = 3;
+            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 404F));
+            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel4.Controls.Add(this.pictureBox1, 1, 0);
+            this.tableLayoutPanel4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel4.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel4.Name = "tableLayoutPanel4";
+            this.tableLayoutPanel4.RowCount = 1;
+            this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel4.Size = new System.Drawing.Size(782, 379);
+            this.tableLayoutPanel4.TabIndex = 2;
+            this.tableLayoutPanel4.SizeChanged += new System.EventHandler(this.tableLayoutPanel4_SizeChanged);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // закрытьВсеToolStripMenuItem
+            // 
+            this.закрытьВсеToolStripMenuItem.Name = "закрытьВсеToolStripMenuItem";
+            this.закрытьВсеToolStripMenuItem.Size = new System.Drawing.Size(194, 22);
+            this.закрытьВсеToolStripMenuItem.Text = "Закрыть все";
+            this.закрытьВсеToolStripMenuItem.Click += new System.EventHandler(this.закрытьВсеToolStripMenuItem_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(192, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(398, 373);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(788, 564);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.MinimumSize = new System.Drawing.Size(537, 594);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel4.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem открытьФотоToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem закрытьToolStripMenuItem;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Button Next_button;
+        private System.Windows.Forms.Button Prev_button;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel4;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ToolStripMenuItem закрытьВсеToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,275 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+using System.Net;
+
+namespace PhotoSmort
+{
+    public partial class Form1 : Form
+    {
+        private List<Image> list = new List<Image>();
+        private int selected_index;
+        private int slide_index;
+        private int max_slide_index;
+        public Form1()
+        {
+            InitializeComponent();
+
+            selected_index = -1;
+            slide_index = 0;
+            var slider = new ImageItemSlider { Parent = this, Top = 100, Width = ClientSize.Width, Height = 200, Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Right | AnchorStyles.Left };
+            slider.Build(list);
+
+            Button P_prev = new Button() { Parent = this, Text = "Prev", BackColor = Color.White, Top = 50, Name = "B_Prev" };
+
+            Prev_button.Enabled = false;
+            Next_button.Enabled = false;
+
+            Prev_button.Click += delegate 
+            { 
+                slider.CurentSlide--; slide_index--; if (slide_index == 0) { Prev_button.Enabled = false; if (max_slide_index > 0) Next_button.Enabled = true; else Next_button.Enabled = false; }
+            };
+            Next_button.Click += delegate { slider.CurentSlide++; slide_index++; Prev_button.Enabled = true; if (slide_index == max_slide_index) Next_button.Enabled = false; };
+
+            this.tableLayoutPanel3.Controls.Add(slider, 1, 0);
+
+            
+            //var pb = new PictureBox { Parent = this, Top = 400, Left = Width / 2 - 200, Width = 400, Height = 400, SizeMode = PictureBoxSizeMode.Zoom };
+            //this.tableLayoutPanel4.Controls.Add(pb, 1, 0);
+            //pb.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel4.ColumnStyles[1].Width = tableLayoutPanel4.Height;
+
+            slider.ImageClick += delegate 
+            {
+                selected_index = slider.HoveredImageIndex;
+                pictureBox1.Image = list[slider.HoveredImageIndex]; 
+                if (pictureBox1.Image != null)
+                {
+                    float s = float.Parse(Convert.ToString(Convert.ToDouble(pictureBox1.Image.Width) / Convert.ToDouble(pictureBox1.Image.Height)));
+                    this.tableLayoutPanel4.ColumnStyles[1].Width = tableLayoutPanel4.Height * s;
+                }
+            };
+
+        }
+
+        public class ImageItemSlider : Control
+        {
+            public int ImagesPerSlide { get; set; }
+            public int CurentSlide { get; set; }
+            public Padding ImagePaddings { get; set; }
+            private ImageItemDrawer ImageItemDrawer { get; set; }
+            public int AnimateSpeed { get; set; }
+            public int HoveredImageIndex { get; set; }
+            public event EventHandler ImageClick = delegate { };
+            public bool IsAnimatedNow { get; private set; }
+
+            private List<Image> items;
+            private int HScroll;
+
+            public ImageItemSlider()
+            {
+                SetStyle(ControlStyles.AllPaintingInWmPaint | ControlStyles.OptimizedDoubleBuffer | ControlStyles.UserPaint | ControlStyles.ResizeRedraw, true);
+
+                ImagesPerSlide = 5;
+                ImageItemDrawer = new ImageItemDrawer();
+                AnimateSpeed = 30;
+                Cursor = Cursors.Hand;
+
+                Application.Idle += Animate;
+            }
+
+            private void Animate(object sender, EventArgs e)
+            {
+                var expectedHScroll = ClientSize.Width * CurentSlide;
+                if (HScroll != expectedHScroll)
+                {
+                    //var speed = Math.Abs(HScroll - expectedHScroll) <= AnimateSpeed ? 1 : AnimateSpeed;
+                    //HScroll += speed * Math.Sign(expectedHScroll - HScroll);
+                    HScroll += (expectedHScroll - HScroll);
+                    Invalidate();
+                    IsAnimatedNow = true;
+                }
+                else
+                    IsAnimatedNow = false;
+            }
+
+            public void Build(List<Image> items)
+            {
+                this.items = items;
+            }
+
+            protected override void OnPaint(PaintEventArgs e)
+            {
+                var imgWidth = ClientSize.Width / ImagesPerSlide;
+
+                for (int i = 0; i < items.Count; i++)
+                {
+                    var x = i * imgWidth - HScroll;
+                    var rect = new Rectangle(x, ClientRectangle.Top, imgWidth, ClientSize.Height);
+                    if (e.ClipRectangle.IntersectsWith(rect))
+                    {
+                        if (i == HoveredImageIndex)
+                            using (var brush = new LinearGradientBrush(rect, BackColor, Color.Orange, 90))
+                            {
+                                e.Graphics.FillRectangle(brush, rect);
+                            }
+
+                        ImageItemDrawer.Draw(e.Graphics, rect, items[i]);
+                    }
+                }
+            }
+
+            protected override void OnMouseMove(MouseEventArgs e)
+            {
+                base.OnMouseMove(e);
+                if (!IsAnimatedNow)
+                {
+                    HoveredImageIndex = PointToIndex(e.Location);
+                    Invalidate();
+                }
+            }
+
+            protected override void OnMouseDown(MouseEventArgs e)
+            {
+                base.OnMouseDown(e);
+
+                if (!IsAnimatedNow)
+                    if (e.Button == MouseButtons.Left)
+                        if (HoveredImageIndex >= 0 && HoveredImageIndex < items.Count)
+                            ImageClick(this, EventArgs.Empty);
+            }
+
+            /// <summary>
+            /// Returns index of ImageIndex in given point
+            /// </summary>
+            public int PointToIndex(Point p)
+            {
+                var imgWidth = ClientSize.Width / ImagesPerSlide;
+                return (p.X + HScroll) / imgWidth;
+            }
+        }
+
+        public class ImageItemDrawer
+        {
+            public int BottomStripHeight = 30;
+            public Color BottomStripColor = Color.FromArgb(100, 0, 0, 0);
+            public Color BottomStripTextColor = Color.White;
+            public Font BottomStripTextFont = new Font(FontFamily.GenericSansSerif, 12);
+            public Padding Paddings = new Padding(5, 2, 5, 2);
+
+            public virtual void Draw(Graphics gr, Rectangle rect, Image item)
+            {
+                //image rect with paddings
+                rect = new Rectangle(rect.Left + Paddings.Left, rect.Top + Paddings.Top, rect.Width - Paddings.Left - Paddings.Right, rect.Height - Paddings.Top - Paddings.Bottom);
+                //bottom strip rect
+                var strip = new Rectangle(rect.Left, rect.Top + rect.Height - BottomStripHeight, rect.Width, BottomStripHeight);
+                //draw image
+                gr.DrawImage(item, rect);
+                //draw bottom strip
+                using (var brush = new SolidBrush(BottomStripColor))
+                {
+                    gr.FillRectangle(brush, strip);
+                    brush.Color = BottomStripTextColor;
+                    var sf = new StringFormat { LineAlignment = StringAlignment.Center };
+                    //gr.DrawString(item.Comment, BottomStripTextFont, brush, strip, sf);
+                    sf.Alignment = StringAlignment.Far;
+                    //gr.DrawString(item.Duration.ToString(), BottomStripTextFont, brush, strip, sf);
+                }
+            }
+        }
+
+
+
+        //public class ImageItem
+        //{
+        //    public Image Image { get; set; }
+        //}
+
+        private void tableLayoutPanel4_SizeChanged(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image != null)
+            {
+                float s = float.Parse(Convert.ToString(Convert.ToDouble(pictureBox1.Image.Width) / Convert.ToDouble(pictureBox1.Image.Height)));
+                this.tableLayoutPanel4.ColumnStyles[1].Width = tableLayoutPanel4.Height*s;
+            }
+        }
+
+        private void открытьФотоToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                list.Add(Image.FromFile(openFileDialog1.FileName));
+
+                //if (list.Count > 5) Next_button.Enabled = true;
+                //else Next_button.Enabled = false;
+
+                int t = list.Count;
+
+                for (;;)
+                {
+                    if (list.Count < 5) break;
+
+                    if (t % 5 == 0)
+                    {
+                        max_slide_index = t / 5;
+                        break;
+                    }
+                    else t--;
+                }
+
+                if (selected_index != max_slide_index) Next_button.Enabled = true;
+            }
+        }
+
+        private void закрытьВсеToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            list.Clear();
+            pictureBox1.Image = null;
+            if (list.Count > 5) Next_button.Enabled = true;
+            else Next_button.Enabled = false;
+
+            int t = list.Count;
+
+            for (; ; )
+            {
+                if (list.Count < 5) break;
+
+                if (t % 5 == 0)
+                {
+                    max_slide_index = t / 5;
+                    break;
+                }
+                else t--;
+            }
+
+            if (selected_index < max_slide_index) Next_button.Enabled = true;
+            if (selected_index > max_slide_index)
+            {
+                Prev_button.Pre
+            }
+        }
+
+        private void закрытьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (selected_index != -1)
+            {
+                list.Remove(list[selected_index]);
+                if (list.Count != 0)
+                {
+                    pictureBox1.Image = list[0];
+                }
+                else pictureBox1.Image = null;
+
+                if (list.Count > 5) Next_button.Enabled = true;
+                else Next_button.Enabled = false;
+            }
+        }
+    }
+}
